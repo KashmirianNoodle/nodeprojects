71ea538333177a3948fef9c63c7cc42c23fd3010
KashmirianNoodle <72066697+KashmirianNoodle@users.noreply.github.com>
2021-10-28 20:59:11 +0530

    level 3 encryption added, used md5 to hash passwords

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const app = express();
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
+// const encrypt = require("mongoose-encryption")
 
 app.use(express.static("public"))
 
@@ -18,7 +19,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -40,7 +41,7 @@ app.get("/register", function (req, res) {
 
 app.post("/register", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     const newUser = new User({
         username: username,
         password: password
@@ -52,7 +53,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({ username: username }, function (err, foundUser) {
         if (!err) {
             if (foundUser) {
