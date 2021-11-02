da68e142401763cb70ddd823966b29531a0fdd29
KashmirianNoodle <72066697+KashmirianNoodle@users.noreply.github.com>
2021-11-02 14:57:48 +0530

    okay set up secrets page, one user has one secret associated with him

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require("express-session");
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-var findOrCreate = require('mongoose-findorcreate')
+const findOrCreate = require('mongoose-findorcreate')
 
 
 const app = express();
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -87,15 +88,40 @@ app.get("/register", function (req, res) {
     res.render("register");
 })
 app.get("/secrets", function (req, res) {
+    User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", { usersWithSecrets: foundUsers })
+            }
+        }
+    })
+})
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
 })
-// app.get("/submit", function (req, res) { 
-//     res.render("submit");
-// })
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret
+    console.log(req.user)
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    })
+})
 
 app.get("/logout", function (req, res) {
     req.logout();
